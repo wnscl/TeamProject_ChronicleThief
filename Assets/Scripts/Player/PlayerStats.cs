a374d52d4cd3c39619db4f3664cbbe00df548d37
DounPark <ehdns1562@gmail.com>
2025-05-12 10:05:31 +0900

    Player #5
    Player #5

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    [Range(1, 200)][SerializeField] private int health = 100;
-    public int Health { get => health; set => health = Mathf.Clamp(value, 0, 200); }
+    [Range(1, 1000)][SerializeField] private int health = 100;
+    public int Health { get => health; set => health = Mathf.Clamp(value, 0, 1000); }
 
     [Range(1f, 10f)][SerializeField] private float speed = 5f;
     public float Speed { get => speed; set => speed = Mathf.Clamp(value, 0, 10); }
@@ -17,5 +17,7 @@ public class PlayerStats : MonoBehaviour
     //플레이어 자체 방어력
     [SerializeField] private float playerDefensePower = 5;
     public float PlayerDefensePower { get => playerDefensePower; set => playerDefensePower = Mathf.Max(0, value); }
+
+    // 무기 스탯 따로 없이 플레이어 자체 스탯으로 대체
 
 }
