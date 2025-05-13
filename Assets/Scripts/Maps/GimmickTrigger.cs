9dbfdab7ec0e2020f74a0abaf938653a05007254
YeonDarm <msangyun29@naver.com>
2025-05-13 15:40:06 +0900

    #17_Stage 기믹 구현 완료(페이드 아웃 추가 예정)
    FloorShake 애니메이션 연동 완료

---
+++ Assets/Scripts/Maps/GimmickTrigger.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class GimmickTrigger : MonoBehaviour
+{
+    private Animator animator;
+    public GameObject[] gimicFloorObj;
+    private bool isTriggered = false;
+    // public int nextFloorIndex = 0;
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !isTriggered)
+        {
+            isTriggered = true;
+            animator.SetTrigger("FloorShake");
+        }
+    }
+
+    void FindGimicObjects() // 애니메이터 연동으로 메서드 실행(FloorShake)
+    {
+        GameObject parentObj = GameObject.Find("Stage1_Floor");
+        if (parentObj != null)
+        {
+            gimicFloorObj = parentObj.GetComponentsInChildren<Transform>(true)
+                .Select(t => t.gameObject)
+                .Where(go => go.CompareTag("Stage1_Floor"))
+                .ToArray();
+            Debug.Log("Stage1_Floor 개수:" + gimicFloorObj.Length + "등록");
+        }
+        else
+        {
+            Debug.Log("부모 오브젝트 찾을 수 없음.");
+        }
+
+        if (gimicFloorObj != null)
+        {
+            StartCoroutine(ActivateChianObject());
+        }
+        else
+        {
+            Debug.Log("gimicObject: null");
+        }
+    }
+
+    IEnumerator ActivateChianObject()
+    {
+        for (int i = 0; i < gimicFloorObj.Length; i++)
+        {
+            if (i > 0)
+            {
+                gimicFloorObj[i - 1].SetActive(false);
+                Debug.Log($"{gimicFloorObj[i - 1].name} 비활성");
+            }
+            gimicFloorObj[i].SetActive(true);
+            Debug.Log($"{gimicFloorObj[i].name} 활성");
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    // Trigger 메서드를 통해 이동할 인덱스를 입력하면 다음 스테이지로 이동할 수 있는 로직.
+    // 기믹 연출로는 별로 좋은 방법은 아닌 듯...
+    // public void ActiveGimic(int newIndex)
+    // {
+    //     if (newIndex >= 0 && newIndex < gimicFloorObj.Length)
+    //     {
+    //         if (gimicFloorObj[currentFloorIndex] != null){
+    //             gimicFloorObj[currentFloorIndex].SetActive(false);
+    //         }
+    //         gimicFloorObj[newIndex].SetActive(true);
+    //         currentFloorIndex = newIndex;
+    //     }
+    //     else
+    //     {
+    //         Debug.LogWarning("잘못된 Floor 인덱스:" + newIndex);
+    //     }
+    // }
+}
