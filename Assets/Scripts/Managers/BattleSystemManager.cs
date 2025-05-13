2dfc730c3927f8974fc6d95e4c2efbe821f414b9
wnscl <wnscl1175@gmail.com>
2025-05-13 17:25:55 +0900

    Change_BSM
    배틀시스템 매니저 개편

--- Assets/Scripts/Managers/BattleSystemManager.cs
+++ Assets/Scripts/Managers/BattleSystemManager.cs
@@ -1,7 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UI;
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
+
+
+enum Stage
+{
+    None,
+    FirstStart,
+    InBattleWave,
+    InReadyWave,
+    GameOver
+}
 
 public interface IBattleEntity
 {
@@ -10,10 +22,21 @@ public interface IBattleEntity
 
 public class BattleSystemManager : MonoBehaviour
 {
+    [Header("Basic Field")]
     [SerializeField] PlayerController player;
+    [SerializeField] MonsterFactory monsterFactory;
 
-    [SerializeField] MonsterAi mob;
-    [SerializeField] int testAtk;
+    [Header("True or False")]
+    [SerializeField] bool isRunning;
+    [SerializeField] bool isFirstGame;
+    [SerializeField] bool isInBattle;
+    [SerializeField] bool isInReady;
+    [SerializeField] bool isGameOver;
+    [SerializeField] int stageTimer;
+
+    [Header("State")]
+    [SerializeField] Stage currentStage;
+    [SerializeField] Stage nextStage;
 
     public static BattleSystemManager Instance { get; private set; }
 
@@ -21,48 +44,161 @@ public class BattleSystemManager : MonoBehaviour
     {
         Instance = this;
         player = FindObjectOfType<PlayerController>();
-    }
+        monsterFactory = GetComponentInChildren<MonsterFactory>();
 
+
+        nextStage = Stage.FirstStart;
+        isRunning = true;
+        isFirstGame = true;
+        isInBattle = false;
+        isInReady = false;
+        isGameOver = false;
+
+    }
 
     public void AttackOther(IBattleEntity attacker, IBattleEntity target)
     {
-        float attackPower = 10f; // fallback
-        // attacker가 플레이어인 경우
-        if (attacker != null && attacker is MonsterAi)
-        {
-            mob = attacker as MonsterAi;
-        }
-
+        float attackPower= 0;
+        
         if (attacker is PlayerController playerAttacker)
+        // attacker가 플레이어인 경우
+        // 몬스터가 target
         {
             var stats = playerAttacker.GetComponent<PlayerStats>();
             attackPower = stats.PlayerAttackPower;
         }
 
-        // attacker가 몬스터인 경우
-        else if (attacker is MonsterAi monsterAttacker)
-        {
-            testAtk = monsterAttacker.Atk;
-            attackPower = monsterAttacker.Atk; // protected라서 public 프로퍼티 추가 필요함 public int Atk => atk;
-        }
-
         float finalDamage = attackPower;
-
-        // 타겟이 플레이어시 방어력 적용
-        if (target is PlayerController playerTarget)
-        {
-            var stats = playerTarget.GetComponent<PlayerStats>();
-            float defense = stats.PlayerDefensePower;
-
-            finalDamage = attackPower * (100f / (100f + defense));
-            finalDamage = Mathf.Max(1, finalDamage);
-        }
 
         target.TakeDamage(attacker, Mathf.RoundToInt(finalDamage));
     }
-
     public void AttackPlayer(int dmg)
     {
-        player.TakeDamage(dmg);
+        float finalDamage;
+        float attackPower = dmg;
+
+        var stats = player.GetComponent<PlayerStats>();
+        float defense = stats.PlayerDefensePower;
+
+        finalDamage = attackPower * (100f / (100f + defense));
+        finalDamage = Mathf.Max(1, finalDamage);
+        player.TakeDamage(Mathf.RoundToInt(finalDamage));
     }
+
+
+
+
+
+    protected IEnumerator StageRepeater()
+    {
+        Debug.Log("스테이지 머신 가동");
+        while (isRunning)
+        {
+            currentStage = nextStage;
+            switch (currentStage)
+            {
+                case Stage.FirstStart:
+
+
+                    break;
+
+                case Stage.InBattleWave:
+                    yield return StartCoroutine(BattleWave());
+                    nextStage = DecideNextStage();
+                    break;
+
+                case Stage.InReadyWave:
+                    yield return StartCoroutine(ReadyWave());
+                    nextStage = DecideNextStage();
+                    break;
+
+                case Stage.GameOver:
+                    
+
+                    break;
+            }
+        }
+    }
+    private Stage DecideNextStage()
+    {
+        if (CheckGameOver())
+        {
+            return Stage.GameOver;
+        }
+
+        if (stageTimer <= 0 && currentStage == Stage.InBattleWave)
+        {
+            return Stage.InReadyWave;
+        }
+
+        if (stageTimer <= 0 && currentStage == Stage.InReadyWave)
+        {
+            return Stage.InBattleWave;
+        }
+
+        return Stage.None;
+    }
+    private bool CheckGameOver()
+    {
+        if (player.IsDead && isInBattle)
+        {
+            isGameOver = true;
+        }
+
+        return isGameOver;
+    }
+
+    private IEnumerator BattleWave()
+    {
+        isInBattle = true;
+        stageTimer = 60;
+
+        while (stageTimer > 0)
+        {
+            stageTimer--;
+            //UIManager.Instance.시간흐르는메서드호출(stageTimer)
+            if (stageTimer % 10 == 0)
+            {
+                monsterFactory.OnMakeMonster();
+                
+            }
+
+            if (CheckGameOver())
+            {
+                yield break;
+            }
+
+            yield return new WaitForSeconds(1);
+        }
+
+        isInBattle = false;
+        yield break;
+    }
+
+    private IEnumerator ReadyWave()
+    {
+        isInReady = true;
+        stageTimer = 60;
+
+        while (stageTimer > 0)
+        {
+            stageTimer--;
+
+            if (stageTimer % 10 == 0)
+            {
+                monsterFactory.OnMakeMonster();
+            }
+            yield return new WaitForSeconds(1);
+        }
+
+        isInReady = false;
+        yield break;
+    }
+
+    private IEnumerator BossWave()
+    {
+
+        yield break ;
+    }
+
 }
